50da46357ecc2cedfb0c616e13e3ec11ced5d3dd
Rob Anderson <robster31@googlemail.com>
2018-06-16 11:00:37 +0100

    :star: Made webfonts load asynchronously

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,11 @@ import '../sass/index.sass'
 import brandIcons from '@fortawesome/fontawesome-free-brands'
 import fontawesome from '@fortawesome/fontawesome'
 
+import WebFont from 'webfontloader'
+
 // Register icons & setup fontawesome
 fontawesome.library.add(brandIcons)
 fontawesome.dom.i2svg()
+
+// Load web fonts
+WebFont.load({ google: { families: [ 'Montserrat:200', 'Poppins' ] } })
